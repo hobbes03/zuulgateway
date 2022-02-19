c2a56fec7bc3857b49b8e2dc5ebf294e076f7862
Gautam Agrawal <gautam@Gautams-MacBook-Air.local>
2022-02-19 20:44:58 +0530

    Initial commit

---
+++ src/main/java/com/atc/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.atc.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+
+}
